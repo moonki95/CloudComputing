1e8c821c98bffed3d1d796b7f71c9fddf6915572
moonki95 <38067639+moonki95@users.noreply.github.com>
2019-12-07 00:34:51 +0900

    하이퍼링크 기능 추가

--- danggn.py
+++ danggn.py
@@ -1,13 +1,21 @@
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
 
-html = urlopen('https://www.daangn.com/search/%EC%97%90%EC%96%B4%ED%8C%9F')
-bsobj = BeautifulSoup(html,"html.parser")
+def crawling():
+    html = urlopen('https://www.daangn.com/search/%EC%97%90%EC%96%B4%ED%8C%9F')
+    bsobj = BeautifulSoup(html, "html.parser")
+    link=[]
+    cont = bsobj.find("div", {"class": "result-container"})
+    namelist = cont.findAll("span", {"class": "article-title"})
+    addresslist = cont.findAll("p", {"class": "article-region-name"})
+    pricelist = cont.findAll("p", {"class": "article-price"})
+    hyperlink = bsobj.findAll("a", {"class" : "flea-market-article-link"})
 
-namelist = bsobj.findAll("span", {"class":"article-title"})
-addresslist = bsobj.findAll("p",{"class" :"article-region-name"})
-pricelist = bsobj.findAll("p",{"class" : "article-price"})
+    for hyperl in hyperlink:
+        if 'href' in hyperl.attrs:
+            link.append(hyperl.attrs['href'])
 
-for i,j,k in zip(namelist, addresslist, pricelist):
-    print(i.get_text() + j.get_text() + k.get_text())
+    for i,j,k,l in zip(namelist, addresslist, pricelist, hyperlink):
+        print(i.get_text() + j.get_text() + k.get_text() + '' +l.get_text())
 
+crawling()
