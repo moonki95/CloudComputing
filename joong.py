758ad348c000f06e46cca64692d980d36461dcdb
moonki95 <38067639+moonki95@users.noreply.github.com>
2019-12-08 14:59:50 +0900

    Update joong.py

--- joong.py
+++ joong.py
@@ -14,8 +14,8 @@ driver = webdriver.Chrome(executable_path=r"C:\Users\mkjan\PycharmProjects\ccpp\
 driver.implicitly_wait(3)
 driver.get('https://nid.naver.com/nidlogin.login')
 # 아이디/비밀번호를 입력해준다.
-driver.find_element_by_name('id').send_keys('mkjang0905')
-driver.find_element_by_name('pw').send_keys('wkdansrl12!')
+driver.find_element_by_name('id').send_keys('id')
+driver.find_element_by_name('pw').send_keys('pw')
 driver.find_element_by_xpath('//*[@id="frmNIDLogin"]/fieldset/input').click()
 
 driver.maximize_window()
